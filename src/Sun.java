b0fe9f77d05c9253b398f373f1e5cc8d4d746f0c
Michael Fakliier <66269536+MichaelParker007@users.noreply.github.com>
2023-01-19 21:13:50 +0100

    Michael

--- src/Sun.java
+++ src/Sun.java
@@ -8,7 +8,6 @@ import java.awt.Graphics;
  * @author Fouad Ahsayni, Mykhailo Fakliier, Artur Konkel
  * @version 1.0
  */
-
 public class Sun {
 	final static private Color SUN_COLOR_DAY = Color.YELLOW;
 	final static private Color SUN_COLOR_NIGHT = Color.WHITE;
@@ -18,6 +17,12 @@ public class Sun {
 	int radius;
 	boolean dayTime;
 
+	/**
+	 * Constructor is responsible for the position x, position y and radius of the sun
+	 * @param x
+	 * @param y
+	 * @param radius
+	 */
 	public Sun(int x, int y, int radius) {
 		this.x = x;
 		this.y = y;
@@ -25,6 +30,13 @@ public class Sun {
 		this.dayTime = true;
 	}
 
+	/**
+	 * The method switchTime is responsible for the click on the sun. There is a formula for counting the area of
+	 * the sun in the whole JFrame.
+	 * @param x
+	 * @param y
+	 * @return
+	 */
 	public boolean switchTime(int x, int y) {
 
 		if ((x >= this.x && x <= this.x + radius * 2) && (y >= this.y && y <= this.y + radius * 2)) {
@@ -32,22 +44,27 @@ public class Sun {
 			dayTime = !dayTime;
 		}
 
-		return dayTime;
+		return dayTime;			// the method returns the dayTime (night or day)
 	}
 
+	/**
+	 * The method draw is responsible for the drawing of the sun and moon (depends of the dayTime)
+	 * @param g
+	 * @param skyColor
+	 */
 	public void draw(Graphics g, Color skyColor) {
 		if (dayTime) {
 			// Sun
-			g.setColor(SUN_COLOR_DAY);
-			g.fillOval(x, y, radius, radius);
+			g.setColor(SUN_COLOR_DAY);				// setting of the color
+			g.fillOval(x, y, radius, radius);		// setting of the form of the sun
 
 		} else {
 			// Moon
-			g.setColor(SUN_COLOR_NIGHT);
-			g.fillOval(x, y, radius, radius);
+			g.setColor(SUN_COLOR_NIGHT);			// setting of the color
+			g.fillOval(x, y, radius, radius);		// setting of the form of the moon
 
-			g.setColor(skyColor);
-			g.fillOval(x - 20, y, radius, radius);
+			g.setColor(skyColor);					// setting of the color of half moon
+			g.fillOval(x - 20, y, radius, radius);	// setting of the form of the half moon
 		}
 	}
 
